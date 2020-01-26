5a81818cf5927de72be151c26a1b62a87abb532e
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-01-25 23:16:10 -0300

    Movendo index.js para o diretório src

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/', (request, response) => {
-    console.log(request.query)
-    return response.json({
-        message: "teste"
-    })
-})
-app.listen(3333)
